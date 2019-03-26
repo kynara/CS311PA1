dbec1ce463d4b287cc8adc707e4f6118c7b64d7f
Maxwell Talley <max.talley0@gmail.com>
2019-03-26 12:40:38 -0500

    Added methods to Endpoint

---
+++ src/Endpoint.java
@@ -0,0 +1,30 @@
+/**
+ * Project 1
+ * Created by Maxwell Talley, Kynara Fernandes, and Garrett Greenfield
+ * for Com S 311 Spring 2019
+ */
+
+
+public class Endpoint {
+    /**
+     * Value of this Endpoint
+     */
+    private int value;
+
+    /**
+     * Constructor for Endpoint
+     */
+    public Endpoint() {
+
+    }
+
+    /**
+     * Get the Endpoint value
+     * Ex: if this Endpoint represents the left Endpoint of the
+     * interval [1,3], this would return 1.
+     * @return value of this Endpoint
+     */
+    public int getValue() {
+        return this.value;
+    }
+}
