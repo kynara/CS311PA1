389ea9fb8db583570c8292e232e7a306b848a6fe
Garrett Greenfield <greenfield@gjg.student.iastate.edu>
2019-03-26 21:19:20 -0500

    whoooaa kynara smart + tests

--- src/tests.java
+++ src/tests.java
@@ -54,5 +54,33 @@ public class tests {
         assertEquals(expected, tester.method(10, 0), "notes");
 
     }
+
+
+    POSSIBLE ASSERTS
+    assertTrue([message,] boolean condition)
+    assertFalse([message,] boolean condition)
+    assertEquals([message], expected, actual, tolerance(optional))
+    assertNull([message,] object)
+    assertNotNull([message,] object)
+    assertSame([message,] expected, actual)
+    assertNotSame([message,] expected, actual)
+
     */
+
+    @Before
+    public void setupRBTree(){
+        //set this up for things that need to be created for the tests you make below
+
+    }
+
+    @Test
+    public void testName() {
+        RBTree tester = new RBTree(); // the class Node is tested
+
+        // assert statements
+        assertEquals(expected, tester.method(10, 0), "notes");
+
+    }
+
+
 }
