162f00ed9082d656110acff0c18f674918a3f1b3
Maxwell Talley <max.talley0@gmail.com>
2019-03-27 17:39:58 -0500

    Added `type` field to Endpoint

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -14,6 +14,16 @@ public class Endpoint {
     private int value;
 
     /**
+     * The type of Endpoint that this is.
+     * 0 for START
+     * 1 for END
+     */
+    private int type;
+
+    private static final int START = 0;
+    private static final int END = 1;
+
+    /**
      * Constructor for Endpoint
      */
     public Endpoint(int value) {
@@ -21,7 +31,7 @@ public class Endpoint {
     }
 
     /**
-     * Get the Endpoint value PENIS
+     * Get the Endpoint value
      * Ex: if this Endpoint represents the left Endpoint of the
      * interval [1,3], this would return 1.
      * @return value of this Endpoint
@@ -29,4 +39,12 @@ public class Endpoint {
     public int getValue() {
         return this.value;
     }
+
+    /**
+     * Get the type of this Endpoint
+     * Either: 0 for START Endpoint
+     *
+     * @return type of this Endpoint
+     */
+    public int getType() { return this.type; }
 }
