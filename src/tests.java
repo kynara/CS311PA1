89faccff4a81a0aa62b73a855ae85d536258edf3
Garrett Greenfield <greenfield@gjg.student.iastate.edu>
2019-03-26 20:50:16 -0500

    whoooaa kynara smart + tests

---
+++ src/tests.java
@@ -0,0 +1,58 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class tests {
+
+    /*
+    EXPLANATION OF JUNIT TESTS
+    @Test
+    public void testName() {
+        Node tester = new Node(); // the class Node is tested
+
+        // assert statements
+        assertEquals(expected-output, tester.method-to-be-tested(10, 0), "notes to be sent to user");
+
+    }
+
+    ----COPYABLE----
+    TESTS FOR NODE CLASS
+    @Test
+    public void testName() {
+        Node tester = new Node(); // the class Node is tested
+
+        // assert statements
+        assertEquals(expected, tester.method(10, 0), "notes");
+
+    }
+
+    TESTS FOR RBTREE
+    @Test
+    public void testName() {
+        RBTree tester = new RBTree(); // the class Node is tested
+
+        // assert statements
+        assertEquals(expected, tester.method(10, 0), "notes");
+
+    }
+
+    TESTS FOR ENDPOINT
+    @Test
+    public void testName() {
+         Endpoint tester = new Endpoint(); // the class Node is tested
+
+        // assert statements
+        assertEquals(expected, tester.method(10, 0), "notes");
+
+    }
+
+    TESTS FOR INTERVALS
+    @Test
+    public void testName() {
+         Intervals tester = new Intervals(); // the class Node is tested
+
+        // assert statements
+        assertEquals(expected, tester.method(10, 0), "notes");
+
+    }
+    */
+}
