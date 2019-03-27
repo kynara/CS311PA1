a68a6600e74196a14ef5a1257e990d3b650cd707
Maxwell Talley <max.talley0@gmail.com>
2019-03-26 20:07:13 -0500

    Filled out methods in Node

--- src/Node.java
+++ src/Node.java
@@ -8,22 +8,79 @@
  * Represents a node in the RBTree.
  */
 public class Node {
+    /**
+     * Parent of the given Node
+     */
     private Node parent;
+
+    /**
+     * Left child of the given Node
+     */
     private Node left;
+
+    /**
+     * Right child of the given Node
+     */
     private Node right;
+
+    /**
+     * Unique integer used as the Node's key.
+     * Used to sort the red black tree.
+     */
     private int key;
+
+    /**
+     *
+     */
     private int p;
+
+    /**
+     * Sum of p-values in the Node's subtree,
+     * including itself
+     */
     private int val;
+
+    /**
+     * Maximum 'val' in the Node's subtree,
+     * including itself
+     */
     private int maxval;
+
+    /**
+     * The Endpoint associated with the given
+     * Node.
+     */
     private Endpoint ep;
+
+    /**
+     * The Endpoint with the maximum overlap
+     */
     private Endpoint emax;
+
+    /**
+     * The color of the Node.
+     * Either red or black, denoted as
+     * 0 = red
+     * 1 = black
+     */
     private int color;
+    private static final int RED = 0;
+    private static final int BLACK = 1;
 
     /**
      * Constructor for a Node
      */
-    public Node() {
-
+    public Node(Endpoint ep) {
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+        this.key = ep.getValue();
+        this.p = 0; // TODO
+        this.val = 0; // TODO
+        this.maxval = 0;
+        this.ep = ep;
+        this.emax = null; // TODO
+        this.color = 0; // TODO
     }
 
     /**
@@ -31,7 +88,7 @@ public class Node {
      * @return parent Node
      */
     public Node getParent() {
-        return null;
+        return this.parent;
     }
 
     /**
@@ -39,7 +96,7 @@ public class Node {
      * @return left child
      */
     public Node getLeft() {
-        return null;
+        return this.left;
     }
 
     /**
@@ -47,7 +104,7 @@ public class Node {
      * @return right child
      */
     public Node getRight() {
-        return null;
+        return this.right;
     }
 
     /**
@@ -55,7 +112,7 @@ public class Node {
      * @return key
      */
     public int getKey() {
-        return 0;
+        return this.key;
     }
 
     /**
@@ -63,7 +120,7 @@ public class Node {
      * @return the value of p
      */
     public int getP() {
-        return 0;
+        return this.p;
     }
 
     /**
@@ -71,7 +128,7 @@ public class Node {
      * @return val
      */
     public int getVal() {
-        return 0;
+        return this.val;
     }
 
     /**
@@ -79,7 +136,7 @@ public class Node {
      * @return maxval
      */
     public int getMaxVal() {
-        return 0;
+        return this.maxval;
     }
 
     /**
@@ -87,7 +144,7 @@ public class Node {
      * @return this Node's Endpoint
      */
     public Endpoint getEndpoint() {
-        return null;
+        return this.ep;
     }
 
     /**
@@ -97,7 +154,7 @@ public class Node {
      * @return Endpoint representing emax
      */
     public Endpoint getEmax() {
-        return null;
+        return this.emax;
     }
 
     /**
@@ -105,6 +162,6 @@ public class Node {
      * @return int representing a color
      */
     public int getColor() {
-        return -1;
+        return this.color;
     }
 }
